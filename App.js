dc08f7bdc27560503951fbc016527a5345f04aa4
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2020-12-22 20:33:33 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -3,14 +3,13 @@ import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View>
+      <View style={styles.results}>
+
+      </View>
+      <View style={styles.buttons}>
+        
+      </View>
     </View>
   );
 }
