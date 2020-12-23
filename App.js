3d108c1034615829011c8d523c14d5ce42218f49
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2020-12-23 20:20:57 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import {useState} from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import { Entypo } from '@expo/vector-icons';
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const buttons = ['AC', 'DEL', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 3, 2, 1, '+', 0, '.', '+/-', '=']  
+  const [currentNumber, setCurrentNumber] = useState("")
+  const [lastNumber, setLastNumber] = useState("")
+
+  function calcular(){
+    
+  }
+
   return (
     <View>
       <View style={styles.results}>
-
+        <TouchableOpacity style={styles.temaBotao}>
+          <Entypo name={darkMode ? "light-up" : "moon"} size={24} color={darkMode ? "white" : 'black'} onPress={() => darkMode ? setDarkMode(false) : setDarkMode(true)} /> 
+        </TouchableOpacity>
+        <Text style={styles.historiaTexto}>{lastNumber}</Text>
+        <Text style={styles.resulTexto}>{currentNumber}</Text>
       </View>
       <View style={styles.buttons}>
         
